8205fe3025daa63838d697af1ce53f9d4b4556a5
PeterAsenov22 <peter12345@abv.bg>
2019-03-12 12:47:28 +0200

    rename key types

--- src/app/core/enums/key-type.ts
+++ src/app/core/enums/key-type.ts
@@ -1,4 +1,4 @@
 export enum KeyType {
-  Ed25519 = 'Ed25519VerificationKey2018',
-  Secp256k1 = 'Secp256k1VerificationKey2018'
+  Ed25519 = 'Ed25519VerificationKey',
+  Secp256k1 = 'Secp256k1VerificationKey'
 }
