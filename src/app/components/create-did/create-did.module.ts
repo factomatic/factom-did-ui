14244fd2251cc15297711b35a8481ab838db7994
PeterAsenov22 <peter12345@abv.bg>
2019-03-14 18:48:38 +0200

    add angular-highlight-js

--- src/app/components/create-did/create-did.module.ts
+++ src/app/components/create-did/create-did.module.ts
@@ -1,10 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HighlightJsModule, HIGHLIGHT_JS } from 'angular-highlight-js';
+import hljs from 'highlight.js/lib/highlight';
+import json from 'highlight.js/lib/languages/json';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { NgModule } from '@angular/core';
 
 import { createComponents } from '.';
 import { CreateDIDRoutingModule } from './create-did.routing';
+
+hljs.registerLanguage('json', json);
+
+export function highlightJsFactory() {
+  return hljs;
+}
 
 @NgModule({
   declarations: [
@@ -14,6 +23,10 @@ import { CreateDIDRoutingModule } from './create-did.routing';
     CommonModule,
     CreateDIDRoutingModule,
     FormsModule,
+    HighlightJsModule.forRoot({
+      provide: HIGHLIGHT_JS,
+      useFactory: highlightJsFactory
+    }),
     MDBBootstrapModule.forRoot(),
     ReactiveFormsModule
   ],
