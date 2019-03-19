02b049c6a1336b243998f26f3f84b001bacb17f8
PeterAsenov22 <peter12345@abv.bg>
2019-03-19 23:17:07 +0200

    add tooltip

--- src/app/components/create-did/create-did.module.ts
+++ src/app/components/create-did/create-did.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
+import { createComponents } from '.';
+import { CreateDIDRoutingModule } from './create-did.routing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HighlightJsModule, HIGHLIGHT_JS } from 'angular-highlight-js';
 import hljs from 'highlight.js/lib/highlight';
 import json from 'highlight.js/lib/languages/json';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
-
-import { createComponents } from '.';
-import { CreateDIDRoutingModule } from './create-did.routing';
 
 hljs.registerLanguage('json', json);
 
@@ -28,6 +28,7 @@ export function highlightJsFactory() {
       useFactory: highlightJsFactory
     }),
     MDBBootstrapModule.forRoot(),
+    NgbTooltipModule,
     ReactiveFormsModule
   ],
   exports: [
