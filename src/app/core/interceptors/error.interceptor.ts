d02fa42bce849e0f021570c5500687f320d3ca12
PeterAsenov22 <peter12345@abv.bg>
2019-03-29 19:28:40 +0200

    update error interceptor

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -1,4 +1,4 @@
-import { catchError } from 'rxjs/operators';
+import { catchError, retryWhen, take, concat, delay } from 'rxjs/operators';
 import {
   HttpRequest,
   HttpHandler,
@@ -11,29 +11,24 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { Observable, throwError } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 
-import { DIDService } from '../services/did.service';
-
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
   constructor (
-    private didService: DIDService,
     private toastr: ToastrService,
     private spinner: NgxSpinnerService ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next
       .handle(req)
-      .pipe(catchError((err: HttpErrorResponse) => {
-        if (err.status === 503) {
-          this.didService.recordOnChain();
-        } else {
-          this.spinner.hide();
+      .pipe(
+        retryWhen(errors => errors.pipe(delay(1000), take(3), concat(throwError(errors)))),
+        catchError((err: HttpErrorResponse) => {
+        this.spinner.hide();
 
-          const errorMessage = err.error.message ? err.error.message : err.message;
-          this.toastr.error(errorMessage, 'Warning!');
-        }
+        const errorMessage = 'A problem occurred while recording your DID. Please, try again!';
+        this.toastr.error(errorMessage, 'Warning!');
 
         return throwError(err);
       }));
-  }
+    }
 }
