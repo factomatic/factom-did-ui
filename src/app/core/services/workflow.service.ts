347ff6872a9f4abd643b96df7ef4d45e70cfa280
PeterAsenov22 <peter12345@abv.bg>
2019-05-06 13:46:25 +0300

    add moveToNextStep and moveToPreviousStep functions to workflow service

--- src/app/core/services/workflow.service.ts
+++ src/app/core/services/workflow.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store, select } from '@ngrx/store';
 
+import { actionRoutes } from '../enums/action-routes';
 import { AppState } from '../store/app.state';
+import { MoveToStep } from '../store/action/action.actions';
 
 @Injectable()
 export class WorkflowService {
   private selectedAction: string;
   private currentStepIndex: number;
+  private selectedActionRoutes;
 
-  constructor (private store: Store<AppState>) {
+  constructor (
+    private router: Router,
+    private store: Store<AppState>) {
     this.store.pipe(select(state => state.action))
       .subscribe(action => {
         this.selectedAction = action.selectedAction;
         this.currentStepIndex = action.currentStepIndex;
+
+        if (this.selectedAction) {
+          this.selectedActionRoutes = actionRoutes[this.selectedAction];
+        }
       });
   }
 
@@ -22,5 +32,17 @@ export class WorkflowService {
 
   getCurrentStepIndex(): number {
     return this.currentStepIndex;
+  }
+
+  moveToNextStep(queryParams?: any) {
+    const nextStepIndex = this.currentStepIndex + 1;
+    this.store.dispatch(new MoveToStep(nextStepIndex));
+    this.router.navigate([this.selectedActionRoutes[nextStepIndex]], queryParams);
+  }
+
+  moveToPreviousStep() {
+    const previousStepIndex = this.currentStepIndex - 1;
+    this.store.dispatch(new MoveToStep(previousStepIndex));
+    this.router.navigate([this.selectedActionRoutes[previousStepIndex]]);
   }
 }
