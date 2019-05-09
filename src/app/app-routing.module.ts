0f452ccab944fb7f3f1dea66ee4599580073b0ec
PeterAsenov22 <peter12345@abv.bg>
2019-05-09 16:28:26 +0300

    add update-action guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { ActionComponent } from './components/shared/action/action.component';
 import { CreateActionGuard } from './core/guards/create-action.guard';
 import { FinalComponent } from './components/shared/final/final.component';
 import { FinalComponentGuard } from './core/guards/final-component.guard';
+import { UpdateActionGuard } from './core/guards/update-action.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'action' },
   { path: 'action', component: ActionComponent },
   { path: 'create', loadChildren: './components/did/did.module#DIDModule', canActivate: [ CreateActionGuard ] },
+  { path: 'update', loadChildren: './components/did/did.module#DIDModule', canActivate: [ UpdateActionGuard ] },
   { path: 'final', component: FinalComponent, canActivate: [ FinalComponentGuard ] },
   { path: '**', redirectTo: 'action' }
 ];
