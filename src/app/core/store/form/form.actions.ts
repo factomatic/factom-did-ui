3d66269dea4781d1e0f575b719daad93ed6d6ee3
PeterAsenov22 <peter12345@abv.bg>
2019-05-16 09:57:05 +0300

    add UpdateAuthenticationKey action

--- src/app/core/store/form/form.actions.ts
+++ src/app/core/store/form/form.actions.ts
@@ -8,6 +8,7 @@ export const ADD_SERVICE = '[FORM] ADD_SERVICE';
 export const ADD_ORIGINAL_AUTHENTICATION_KEYS = '[FORM] ADD_ORIGINAL_AUTHENTICATION_KEYS';
 export const ADD_ORIGINAL_PUBLIC_KEYS = '[FORM] ADD_ORIGINAL_PUBLIC_KEYS';
 export const ADD_ORIGINAL_SERVICES = '[FORM] ADD_ORIGINAL_SERVICES';
+export const UPDATE_AUTHENTICATION_KEY = '[FORM] UPDATE_AUTHENTICATION_KEY';
 export const UPDATE_PUBLIC_KEY = '[FORM] UPDATE_PUBLIC_KEY';
 export const REMOVE_AUTHENTICATION_KEY = '[FORM] REMOVE_AUTHENTICATION_KEY';
 export const REMOVE_PUBLIC_KEY = '[FORM] REMOVE_PUBLIC_KEY';
@@ -47,6 +48,12 @@ export class AddOriginalServices implements Action {
   readonly type: string = ADD_ORIGINAL_SERVICES;
 
   constructor (public payload: ServiceModel[]) { }
+}
+
+export class UpdateAuthenticationKey implements Action {
+  readonly type: string = UPDATE_AUTHENTICATION_KEY;
+
+  constructor (public payload: KeyModel) { }
 }
 
 export class UpdatePublicKey implements Action {
--- src/app/core/store/form/form.reducers.ts
+++ src/app/core/store/form/form.reducers.ts
@@ -1,6 +1,6 @@
 import { ADD_AUTHENTICATION_KEY, ADD_PUBLIC_KEY, ADD_SERVICE,
   ADD_ORIGINAL_AUTHENTICATION_KEYS, ADD_ORIGINAL_PUBLIC_KEYS, ADD_ORIGINAL_SERVICES,
-  UPDATE_PUBLIC_KEY,
+  UPDATE_AUTHENTICATION_KEY, UPDATE_PUBLIC_KEY,
   REMOVE_AUTHENTICATION_KEY, REMOVE_PUBLIC_KEY, REMOVE_SERVICE } from './form.actions';
 import { CLEAR_FORM } from '../action/action.actions';
 import { FormState } from './form.state';
@@ -61,9 +61,27 @@ function addOriginalService(state: FormState, services: ServiceModel[]) {
   };
 }
 
-function updatePublicKey(state: FormState, key: KeyModel) {
-  const pubKeyIndex = state.publicKeys.findIndex(k => k.publicKey === key.publicKey);
+function updateAuthenticationKey(state: FormState, key: KeyModel) {
+  const authenticationKeys = state.authenticationKeys.slice();
+  const authKeyIndex = authenticationKeys.findIndex(k => k.publicKey === key.publicKey);
+  authenticationKeys[authKeyIndex] = key;
+
   const publicKeys = state.publicKeys.slice();
+  const pubKeyIndex = publicKeys.findIndex(k => k.publicKey === key.publicKey);
+  if (pubKeyIndex > -1) {
+    publicKeys[pubKeyIndex] = key;
+  }
+
+  return {
+    ...state,
+    publicKeys: publicKeys,
+    authenticationKeys: authenticationKeys
+  };
+}
+
+function updatePublicKey(state: FormState, key: KeyModel) {
+  const publicKeys = state.publicKeys.slice();
+  const pubKeyIndex = publicKeys.findIndex(k => k.publicKey === key.publicKey);
   publicKeys[pubKeyIndex] = key;
 
   const authenticationKeys = state.authenticationKeys.slice();
@@ -115,6 +133,8 @@ export function formReducers(state: FormState = initialState, action) {
       return addOriginalPublicKey(state, action.payload);
     case ADD_ORIGINAL_SERVICES:
       return addOriginalService(state, action.payload);
+    case UPDATE_AUTHENTICATION_KEY:
+      return updateAuthenticationKey(state, action.payload);
     case UPDATE_PUBLIC_KEY:
       return updatePublicKey(state, action.payload);
     case CLEAR_FORM:
